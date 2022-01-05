13c72ac290f3e66359c03405d9a6cd0cb6a7a1e6
Guillaume Alvarez <alvarez.guillaume2506@gmail.com>
2022-01-05 12:50:20 +0100

    Avancée TP

---
+++ Tp/src/main.cpp
@@ -0,0 +1,40 @@
+// #include <Arduino.h>
+
+// #define LED 13
+
+// void setup() {
+//     pinMode(LED, OUTPUT);
+// }
+
+// void loop() {
+//     digitalWrite(LED, HIGH);
+//     delay(1000);
+//     digitalWrite(LED, LOW);
+//     delay(1000);
+// }
+//Code pour allumer et éteindre la LED avec les fcts de base Arduino
+
+#include <Arduino.h>
+
+#define LED 13
+
+// #define PORTB
+
+void setup() {
+    pinMode(LED, OUTPUT);
+}
+
+void ledOn() {
+  PORTB = PORTB | (1 << 5);
+}
+
+void ledOff() {
+  PORTB = PORTB & ~ (1 << 5);
+}
+
+void loop() {
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+}
