a24316fe8794f69ee8b6b51582a6adddaa57f6ca
Guillaume Alvarez <alvarez.guillaume2506@gmail.com>
2022-01-13 16:35:29 +0100

    TP allumer/éteindre LED en écrivant nos propores fonctions

--- Tp/src/main.cpp
+++ Tp/src/main.cpp
@@ -14,9 +14,7 @@
 // }
 //Code pour allumer et éteindre la LED avec les fcts de base Arduino
 
-#include <Arduino.h>
-
-#define LED 13
+// #define LED 13
 
 // #define PORTB
 
@@ -24,27 +22,47 @@
 //     pinMode(LED, OUTPUT);
 // }
 
+
+// void ledOn() {
+//   PORTB = PORTB | (1 << 5);//passage du bit 5 à 1
+// }
+
+// void ledOff() {
+//   PORTB = PORTB & ~ (1 << 5);//passage du bit 5 à 0 car AND NOT (...)
+// }
+
+#include <Arduino.h>
+
 //mettre à 1 le bit 5 car la LED qu'on veut allumer correspond au PB5
-void setup() {
+void ledSetup() {
     DDRB =  DDRB | (1 << 5);
-}
-
-void ledOn() {
-  PORTB = PORTB | (1 << 5);//passage du bit 5 à 1
-}
-
-void ledOff() {
-  PORTB = PORTB & ~ (1 << 5);//passage du bit 5 à 0 car AND NOT (...)
 }
 
 void ledToggle() {
     PINB |= (1 << 5);
 }
-void loop() {
+void timerSetup() {
     ledToggle();
     // delay(1000);
     TCCR1A = 0;
-    TCCR1B = 13;//1101
-    TIMSK1 = 2;//10
+    TCCR1B = 13;//1101(1 << WGM12) | (1 << CS12) | (1 << CS10)
+    OCR1A = 15625;
+    TIMSK1 = 2;//10  (1 << OCIE1A)
     //Fréquence / prescaler (1024 ici) --> fréquence 16 000 000 / 1024 = 26625 
+}
+
+//ISR c'est un define, déjà reconnu par le compilateur
+//paramètres TIMER1 c'est la syntaxe de vecteur d'interruption
+//interrutio, et micro-controleur va regarder à quelle adresse il faut faire l'interruption
+ISR(TIMER1_COMPA_vect) { 
+  ledToggle();
+}
+
+void setup() {
+  ledSetup();
+  timerSetup();
+}
+
+void loop() {
+  
 }
